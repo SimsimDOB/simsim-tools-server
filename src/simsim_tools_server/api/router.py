4bc5c4f5b47d50d30d0e096699c8cc9845d12015
FernandoYu <fyu@fi.uba.ar>
2026-01-01 20:05:37 -0300

    feat: remove download endpoint and update pdf_merge response to stream merged PDFs

--- src/simsim_tools_server/api/router.py
+++ src/simsim_tools_server/api/router.py
@@ -5,4 +5,3 @@ from .v1.endpoints import summonses_count, pdf_merge, download
 api_router = APIRouter(prefix="/api/v1")
 api_router.include_router(summonses_count.router)
 api_router.include_router(pdf_merge.router)
-api_router.include_router(download.router)
--- src/simsim_tools_server/api/v1/endpoints/pdf_merge.py
+++ src/simsim_tools_server/api/v1/endpoints/pdf_merge.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, HTTPException, File, UploadFile, status
+from fastapi.responses import FileResponse, StreamingResponse
 from tempfile import NamedTemporaryFile
 import logging
 from pathlib import Path
@@ -13,15 +14,11 @@ async def pdf_merge(files: list[UploadFile] = File(...)):
     try:
         merged_pdf_bytes = await merge_pdfs(files)
 
-        with NamedTemporaryFile(delete=False, suffix=".pdf") as temp_file:
-            temp_file.write(merged_pdf_bytes.getvalue())
-            temp_file.flush()
-            temp_file.seek(0)
-            temp_path = temp_file.name
-            temp_name = Path(temp_path).stem
-            logging.info(f"Merged PDF saved temporarily at: {temp_path}")
-
-        return {"filename": temp_name}
+        return StreamingResponse(
+            merged_pdf_bytes,
+            media_type="application/pdf",
+            headers={"Content-Disposition": 'attachment; filename="merged.pdf"'},
+        )
     except Exception as e:
         logging.error(f"Failed to merge PDFs: {e}")
         raise HTTPException(
